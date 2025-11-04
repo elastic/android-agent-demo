be5f2977dbeab8a1eddc1a32fe0801496959da89
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-11-04 13:49:35 +0100

    Spotless

--- app/src/main/java/co/elastic/otel/android/demo/ui/FirstFragment.kt
+++ app/src/main/java/co/elastic/otel/android/demo/ui/FirstFragment.kt
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.otel.android.demo.ui
 
 import android.os.Bundle
@@ -13,45 +31,46 @@ import co.elastic.otel.android.demo.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-    private val binding get() = _binding!!
+  private var _binding: FragmentFirstBinding? = null
+  private val binding
+    get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?,
+  ): View {
+    _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.buttonFirst.setOnClickListener {
+      val bundle = bundleOf("city" to binding.citySpinner.selectedItem.toString())
+      findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    binding.buttonCrash.setOnClickListener { doCrash() }
 
-        binding.buttonFirst.setOnClickListener {
-            val bundle = bundleOf("city" to binding.citySpinner.selectedItem.toString())
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
-        }
-
-        binding.buttonCrash.setOnClickListener {
-            doCrash()
-        }
-
-        ArrayAdapter.createFromResource(
+    ArrayAdapter.createFromResource(
             view.context,
             R.array.city_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.citySpinner.adapter = adapter
+            android.R.layout.simple_spinner_item,
+        )
+        .also { adapter ->
+          adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+          binding.citySpinner.adapter = adapter
         }
-    }
+  }
 
-    private fun doCrash() {
-        throw RuntimeException("Some crash")
-    }
+  private fun doCrash() {
+    throw RuntimeException("Some crash")
+  }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 }
--- backend/src/main/java/co/elastic/apm/android/demo/backend/data/ForecastResponse.java
+++ backend/src/main/java/co/elastic/apm/android/demo/backend/data/ForecastResponse.java
@@ -1,28 +1,45 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.apm.android.demo.backend.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.Objects;
 
 public class ForecastResponse {
 
-    @JsonProperty("current_weather")
-    CurrentWeatherResponse currentWeather;
+  @JsonProperty("current_weather")
+  CurrentWeatherResponse currentWeather;
 
-    public CurrentWeatherResponse getCurrentWeather() {
-        return currentWeather;
-    }
+  public CurrentWeatherResponse getCurrentWeather() {
+    return currentWeather;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ForecastResponse that = (ForecastResponse) o;
-        return Objects.equals(currentWeather, that.currentWeather);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ForecastResponse that = (ForecastResponse) o;
+    return Objects.equals(currentWeather, that.currentWeather);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(currentWeather);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(currentWeather);
+  }
 }

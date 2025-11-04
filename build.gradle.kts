c7533a897590d0c16fa7098369952061f525730b
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-11-04 14:03:28 +0100

    Attaching spotlessApply to compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+@file:Suppress("UnstableApiUsage")
+
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+
 plugins {
     alias(libs.plugins.androidApp) apply false
     alias(libs.plugins.androidLib) apply false
@@ -18,6 +22,19 @@ subprojects {
             ktfmt()
             target("src/*/java/**/*.kt")
             licenseHeaderFile(rootProject.file("source_license.txt"))
+        }
+    }
+
+    val spotlessApply = tasks.named("spotlessApply")
+    plugins.withId("com.android.application") {
+        val componentsExtension = extensions.getByType(ApplicationAndroidComponentsExtension::class)
+        componentsExtension.onVariants(componentsExtension.selector().all()) {
+            it.lifecycleTasks.registerPreBuild(spotlessApply)
+        }
+    }
+    plugins.withId("java") {
+        tasks.named("classes") {
+            dependsOn(spotlessApply)
         }
     }
 }

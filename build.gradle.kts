b5fe8a91a5feac81176650795dddd01ea65aae6f
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-11-05 10:36:20 +0100

    Ensure manual spotless apply

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-@file:Suppress("UnstableApiUsage")
-
-import com.android.build.api.variant.ApplicationAndroidComponentsExtension
-
 plugins {
     alias(libs.plugins.androidApp) apply false
     alias(libs.plugins.androidLib) apply false
@@ -22,19 +18,6 @@ subprojects {
             ktfmt()
             target("src/*/java/**/*.kt")
             licenseHeaderFile(rootProject.file("source_license.txt"))
-        }
-    }
-
-    val spotlessApply = tasks.named("spotlessApply")
-    plugins.withId("com.android.application") {
-        val componentsExtension = extensions.getByType(ApplicationAndroidComponentsExtension::class)
-        componentsExtension.onVariants(componentsExtension.selector().all()) {
-            it.lifecycleTasks.registerPreBuild(spotlessApply)
-        }
-    }
-    plugins.withId("java") {
-        tasks.named("classes") {
-            dependsOn(spotlessApply)
         }
     }
 }

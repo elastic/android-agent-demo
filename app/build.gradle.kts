11b96f3101c0d487e60a3a7dbc2578b04e335bb0
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2026-01-22 22:02:16 +0100

    Bumping AGP to 9.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("co.elastic.otel.android.agent") version "1.5.0"
     id("co.elastic.otel.android.instrumentation.okhttp") version "1.5.0"
 }

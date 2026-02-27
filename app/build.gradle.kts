1c736d94e11290e34b07e9ade0bed21bf1d4bcaa
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2026-02-27 17:27:14 +0100

    Updating the proguard rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            proguardFiles("proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs["debug"]
         }
     }
